234ee7f7445c3ad292a180b662ef3bb410de590e
Andrew Bruce <andrewbruce.himni@gmail.com>
2024-11-23 04:42:17 -0800

    archive up to date, 2009-2024

--- data-raw/nucc_pins.R
+++ data-raw/nucc_pins.R
@@ -24,12 +24,36 @@ read_clean_csvs <- function(x) {
       dplyr::any_of(cols))
 }
 
-ark_taxonomy <- purrr::map(cleaned_csvs[1:20], read_clean_csvs) |>
-  purrr::list_rbind()
+ark_taxonomy <- purrr::map(cleaned_csvs, read_clean_csvs) |>
+  purrr::list_rbind() |>
+  dplyr::arrange(release_date) |>
+  dplyr::mutate(definition = stringr::str_replace(definition, "Definition to come[\\.]{1,5}", "Definition to come."))
 
 pin_update(
   ark_taxonomy,
   name = "ark_taxonomy",
-  title = "NUCC Taxonomy Archive",
-  description = "Health Care Provider Taxonomy Code Set Archive"
+  title = "NUCC Taxonomy Archive 2009-2024",
+  description = "Health Care Provider Taxonomy Code Set Archive 2009-2024"
 )
+
+
+
+
+# ARCHIVE
+archive::archive_write_files(
+  archive = here::here("data-raw/clean/nucc_taxonomy.tar.xz"),
+  files = c(
+    glue::glue("{here::here()}/data-raw/raw/infotable.csv"),
+    fs::dir_info(glue::glue("{here::here()}/data-raw/clean/csvs"))$path
+  )
+)
+
+fs::dir_delete(glue::glue("{here::here()}/data-raw/clean/csvs"))
+
+archive::archive(file = here::here("data-raw/clean/nucc_taxonomy.tar.xz"))
+
+readr::read_csv(
+  file = archive::archive_read(archive = here::here("data-raw/clean/nucc_taxonomy.tar.xz"), file = 2L),
+  show_col_types = FALSE,
+  col_types = "c",
+  name_repair = janitor::make_clean_names)
